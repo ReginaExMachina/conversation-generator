7178206542421ba09a1dfd5fd573a15046b50d14
ReginaExMachina <we.love.rachel.day.lots@gmail.com>
2016-07-30 00:38:12 -0400

    Login corner setup

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,20 @@
+helpers do 
+	def current_user
+		if session[:user_id]
+			User.find(session[:user_id])
+		end
+	end
+end
+
+############################################################
+
+# MAIN PAGE
+
+# Make @convo variable and method for this
 get '/' do
 	erb(:index)
 end
+
+############################################################
+
+# LOGIN/LOGOUT ACTIONS
